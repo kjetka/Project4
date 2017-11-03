572d712be4f2ef69385e5b77d56d6e93a7c4f272
Unknown <kjetka@uio.no>
2017-11-03 11:33:43 +0100

    ca ferdig metropolis

--- Programs/Project4/main.cpp
+++ Programs/Project4/main.cpp
@@ -12,8 +12,8 @@ using namespace std;
 using namespace arma;
 
 
-void metropolisSampling(int N); //the whole prosess
-void metropolisAlgorithm(mat Matrix);
+//void metropolisSampling(int N); //the whole prosess
+//void metropolisAlgorithm(mat Matrix);
 mat makeMatrix();
 
 std::random_device rd;
@@ -22,25 +22,28 @@ std::mt19937_64 gen(rd());
 std::uniform_real_distribution<double> RandomNumberGenerator(0.0,1.0);
 
 double random_spinn();
-void random_pos(int &ix, int &iy, double size_matrix);
 
 mat makeMicrostate(int L);
 int periodicBC(int i, int limit, int add){
     if ((i+add)>=limit) return 0;
+    if ((i+add) <0) return limit-1;
     else return i+add;
 }
 inline double random_nr(){
     return RandomNumberGenerator(gen);};
 
 int main(){
-    int ix,iy;
-    // Initialize matrix
-    //int spinn = random_();
+
     int L = 2;
+    int MonteCarloCycles = 1e2;
+
+    // Initialize matrix
+
     mat Microstate = makeMicrostate(L);
 
+    // Speedup: when calc many temperatures - use converged state from last Temp as starting microstate
     vec temperatures = vec({1.0, 2.4});
-    int MonteCarloCycles = 2;
+    double Temperature =temperatures[0];
 
     double Energi_state = 0;
     for(int x =0; x < L; x++) {
@@ -49,20 +52,16 @@ int main(){
       }
     }
 
+    vec Acceptance = zeros<mat>(17);
+    for( int de =-8; de <= 8; de+=4) Acceptance(de+8) = exp(-de/Temperature);
 
 
-    //for(int i = 0; i<temperatures.size();i++){
-    double Temperature =temperatures[0];
-    vec EnergyDifference = zeros<mat>(17);
-    for( int de =-8; de <= 8; de+=4) EnergyDifference(de+8) = exp(-de/Temperature);
-
-        //vec Expectationvalues = zeros<vec>(5);
+    //vec Expectationvalues = zeros<vec>(5);
     for(int MC =1; MC<MonteCarloCycles; MC++){
         int local_dEnergy = 0;
          // Flipping state ix,iy
         int ix =random_nr()*L;
         int iy = random_nr()*L;
-        Microstate(ix,iy) = - Microstate(ix,iy);
 
         // This is slow, as periodicBC has an if else loop...
 
@@ -71,11 +70,13 @@ int main(){
                     + Microstate(ix , periodicBC(iy,L,-1))
                     + Microstate(periodicBC(ix,L,1),  iy)
                     + Microstate(periodicBC(ix,L,-1), iy)    );
-        cout <<dE <<endl;
 
-
-
-
+        if (random_nr() <= Acceptance(dE + 8)){
+            Microstate(ix,iy) *= - 1;
+            Energi_state += dE;
+        }
+   }
+    cout << Energi_state<<endl;
 
                 /*
                     (Microstate(ix,PeriodicBoundary(iy,NSpins,-1))+
@@ -93,7 +94,7 @@ int main(){
           }
         }*/
 
-          }
+
 
 
 
@@ -128,18 +129,14 @@ double random_spinn(){
     return number;
 }
 
-void random_pos(int& ix, int &iy, double size_matrix){
-    ix = RandomNumberGenerator(gen)*size_matrix;
-    iy =  RandomNumberGenerator(gen)*size_matrix;
-}
 
-void metropolisSampling(int N){
+/*void metropolisSampling(int N){
     //Matrix = makeMatrix(N);
     //suggest a new move
     //metropolisAlgorithm(Matrix);
     //update averages
 }
-
+*/
 mat makeMicrostate(int L){
     mat microstate = ones<mat>(L,L);
     for (int j=0;j<L;j++){
@@ -151,9 +148,9 @@ mat makeMicrostate(int L){
 return microstate;
 }
 
-void metropolisAlgorithm(mat Matrix){
+/*void metropolisAlgorithm(mat Matrix){
     //for(int i = 0; )
-}
+}*/
 
 /*mat makeMatrix(int N, bool initial){ // true -> ordered initial | false -> random initial
     if(initial == true){
